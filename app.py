0fff2decd6ec129478cc7b1d0284c58d07559687
Joyuv <givanilsonmariz7@outlook.com>
2025-09-19 11:39:41 -0300

    rotas e index

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
+
+@app.route('/login' methods=['POST', 'GET'])
+    return "Nada"
